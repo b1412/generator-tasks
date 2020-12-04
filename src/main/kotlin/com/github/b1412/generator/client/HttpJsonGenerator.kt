1c442d5fd22e8b4af9726d9d146e8d7795d5bf30
Leon Zhou <zhouleib1412@gmail.com>
2020-12-04 17:10:29 +1300

    update

--- src/main/kotlin/com/github/b1412/generator/client/HttpJsonGenerator.kt
+++ src/main/kotlin/com/github/b1412/generator/client/HttpJsonGenerator.kt
@@ -4,7 +4,7 @@ import com.github.b1412.generator.TaskConstants
 import com.github.b1412.generator.entity.CodeProject
 import com.github.b1412.generator.task.SingleTask
 import com.github.b1412.generator.task.Task
-import java.nio.file.Path
+import java.nio.file.Paths
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.div
 
@@ -18,7 +18,7 @@ fun createMap(map: Map<String, Any>): (Task, CodeProject) -> Map<String, Any> {
 
 @ExperimentalPathApi
 class HttpJsonGenerator(map: Map<String, Any>) : SingleTask(
-    filePath = { _, _ -> (Path.of(TaskConstants.generatedPath) / "http/http-client.env.json").toFile().absolutePath },
+    filePath = { _, _ -> (Paths.get(TaskConstants.generatedPath) / "http/http-client.env.json").toFile().absolutePath },
     templatePath = "http/envJson.ftl",
     projectExtProcessors = listOf(createMap(map))
 )
