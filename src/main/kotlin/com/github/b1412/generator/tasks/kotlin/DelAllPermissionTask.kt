d758a79c66b690ba5099a6de7c43ee00df662a36
Leon Zhou <zhouleib1412@gmail.com>
2020-12-30 17:32:57 +1300

    0.1.10

--- src/main/kotlin/com/github/b1412/generator/tasks/kotlin/DelAllPermissionTask.kt
+++ src/main/kotlin/com/github/b1412/generator/tasks/kotlin/DelAllPermissionTask.kt
@@ -4,7 +4,7 @@ import com.github.b1412.generator.TaskConstants
 import com.github.b1412.generator.task.SingleTask
 
 class DelAllPermissionTask(map: MutableMap<String, String>) : SingleTask(
-    filePath = { _, _ -> TaskConstants.generatedPath + "/" + "db/del-permission-all.sql" },
+    filePath = { _, _ -> TaskConstants.generatedPath + "/" + "db/del-crud-permission.sql" },
     templatePath = "kotlin/delAllPermission.ftl",
     projectExtProcessors = listOf(projectPermissionProcessor(map))
 )
